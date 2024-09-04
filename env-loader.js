e84ac069ccfec45ecb976ec17305dfa4b688b084
Rot4tion <nhonhoem0123@gmail.com>
2024-09-04 12:51:06 +0700

    add buyer collection and env

--- env-loader.js
+++ env-loader.js
@@ -3,4 +3,10 @@ import { z } from "zod";
 export const envVariables = z.object({
   /** Mongodb url */
   MONGO_URI: z.string(),
+  DISCORD_TOKEN: z.string(),
+  APPLICATION_ID: z.string(),
+  PUBLIC_KEY: z.string(),
+  GUILD_ID: z.string(),
+  TELEGRAM_TOKEN: z.string(),
+  TELEGRAM_USER_ID: z.string(),
 });
