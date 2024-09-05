ba050d22376a0da2157717f39fe6672e4ae7ca77
Rot4tion <nhonhoem0123@gmail.com>
2024-09-05 09:22:53 +0700

    clean and add schema, add some order type

--- app/routes/api.test.ts
+++ app/routes/api.test.ts
@@ -1,9 +1,14 @@
 import prisma from "@/services/db.server";
 import { LoaderFunctionArgs } from "@remix-run/node";
+import { ActionFunctionArgs, json } from "@remix-run/node";
+
 export async function loader({ request }: LoaderFunctionArgs) {
   if (process.env.NODE_ENV === "production") return;
+  const data = await prisma.buyer.findUnique({
+    where: { id: "66d85f91fac7bb7ba326ba9a" },
+    include: { Orders: { where: { type: "BrawlCoins" } } },
+  });
 
-  const data = await prisma.order.deleteMany({});
   console.log("🚀 ~ loader ~ discordBot.user?:", data);
-  return new Response("ok");
+  return "ok";
 }
