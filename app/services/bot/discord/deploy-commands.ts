7dc19328344cf324bd35f868dd744b86da5d2120
huynq <dodaihoc318@gmail.com>
2024-09-09 21:07:20 +0700

    add InQueue step, remove unnecessary stuffs, fix and add script for deploy commands

--- app/services/bot/discord/deploy-commands.ts
+++ app/services/bot/discord/deploy-commands.ts
@@ -4,21 +4,13 @@ import { commands } from "./commands";
 const commandsData = Object.values(commands).map((command) => command.data);
 
 const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
-// const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_TOKEN);
 
-type DeployCommandsProps = {
-  guildId: string;
-};
-
-export async function deployCommands({ guildId }: DeployCommandsProps) {
+export async function deployCommands() {
   try {
     console.log("Started refreshing application (/) commands.");
 
     await rest.put(
-      Routes.applicationGuildCommands(
-        process.env.DISCORD_APPLICATION_ID,
-        guildId
-      ),
+      Routes.applicationCommands(process.env.DISCORD_APPLICATION_ID),
       {
         body: commandsData,
       }
@@ -30,14 +22,11 @@ export async function deployCommands({ guildId }: DeployCommandsProps) {
   }
 }
 
-// (async (guildId) => {
-//   try {
-//     console.log("Deploying commands to guild...");
-
-//     // Replace 'YOUR_GUILD_ID' with your actual guild ID or make it dynamic
-//     const guildId = process.env.DISCORD_GUILD_ID; // Ensure this is defined in your config
-//     await deployCommands({ guildId });
-//   } catch (error) {
-//     console.error("Error running deployment script:", error);
-//   }
-// })();
+(async () => {
+  try {
+    console.log("Deploying commands ...");
+    await deployCommands();
+  } catch (error) {
+    console.error("Error running deployment script:", error);
+  }
+})();
--- app/services/bot/discord/handler/confirmBtnHandler.ts
+++ app/services/bot/discord/handler/confirmBtnHandler.ts
@@ -31,7 +31,7 @@ export async function ConfirmButtonHandler(interaction: ButtonInteraction) {
       try {
         const dbOrder = await prisma.order.update({
           where: { id: orderId, status: "Pending" },
-          data: { status: "InProcess" },
+          data: { status: "InQueue" },
           select: { id: true, data: true, type: true },
         });
         if (dbOrder.type == "BrawlCoins") {
