21c64832b98efdd407501186e560ff8ee33f0235
Huy Nguyen <huynq@abivin.com>
2024-09-16 17:27:55 +0700

    add: user get balance func

--- app/controller/buyer-controller.ts
+++ app/controller/buyer-controller.ts
@@ -4,9 +4,23 @@ import { z } from "zod";
 
 class BuyerController {
   async getBalance({ platformUserId, platform }: z.infer<typeof GetBalance>) {
-    // const buyer = await prisma.buyer.findUnique({
-    //     where: { platformUserId: platformUserId, platform: platform},
-    //     select: {  },
-    //   });
+    const buyer = await prisma.buyer.findUnique({
+      where: {
+        platform_platformUserId: {
+          platform,
+          platformUserId,
+        },
+      },
+      select: {
+        balance: true,
+      },
+    });
+
+    // Check if buyer exists and return the balance or handle the case where buyer is not found
+    if (buyer) {
+      return { balance: buyer.balance };
+    } else {
+      throw new Error("Buyer not found");
+    }
   }
 }
